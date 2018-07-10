77cc2eb5b04897fee10c13bf3ad3dda2702b46be
Hejia Zhang <hjzh578@gmail.com>
2018-07-09 18:35:00 -0700

    refactor

---
+++ sawyer_safety_checker_cli/sawyer_safety_checker_cli.cpp
@@ -0,0 +1,81 @@
+//
+// Created by hejia on 7/9/18.
+//
+
+#include <ros/ros.h>
+#include <std_msgs/Empty.h>
+#include <sensor_msgs/JointState.h>
+#include <moveit_msgs/PlanningScene.h>
+#include <shape_msgs/SolidPrimitive.h>
+
+#include "sawyer_safety_checker/CollisionChecker.h"
+#include "sawyer_safety_checker/CollisionPredictor.h"
+
+// Store current robot joint states
+sensor_msgs::JointState jointStates;
+
+void onJointStatesMsg(const sensor_msgs::JointState& msg) {
+    // Update jointStates
+    jointStates = msg;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "sawyer_collision_checker_test_node");
+
+    ros::NodeHandle n;
+
+    // Joint state updater.
+    ros::Subscriber subJointStates = n.subscribe("/robot/joint_states", 1, &onJointStatesMsg);
+
+    // Test add collision objects into planning scene
+    ros::Publisher pubAddColObj = n.advertise<moveit_msgs::CollisionObject>("collision_object", 1000);
+    sleep(1.0);
+    moveit_msgs::CollisionObject co;
+    co.id = "table";
+    co.header.frame_id = "sawyer";
+    co.operation = co.ADD;
+    co.primitives.resize(1);
+    co.primitives[0].dimensions.resize(3);
+    co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_X] = 1.09;
+    co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 0.9;
+    co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.1;
+    co.primitive_poses[0].position.x = 0.655;
+    co.primitive_poses[0].position.y = 0;
+    co.primitive_poses[0].position.z = -0.05;
+    co.primitive_poses[0].orientation.x = 0;
+    co.primitive_poses[0].orientation.y = 0;
+    co.primitive_poses[0].orientation.z = 0;
+    co.primitive_poses[0].orientation.w = 1.0;
+    pubAddColObj.publish(co);
+
+    sleep(1);
+
+    // Start collision predicting client.
+    ros::ServiceClient cliCollisionPredictor = n.serviceClient<sawyer_safety_checker::CollisionPredictor>("predict_collision");
+    // Start collision checking client.
+    ros::ServiceClient cliCollisionChecker = n.serviceClient<sawyer_safety_checker::CollisionChecker>("check_collision");
+
+    sawyer_safety_checker::CollisionChecker srvChecker;
+    sawyer_safety_checker::CollisionPredictor srvPredictor;
+
+    // Test Collision Checker
+    std_msgs::Empty req;
+    srvChecker.request.req = req;
+    if (cliCollisionChecker.call(srvChecker)) {
+        ROS_INFO("Collision State: %d", srvChecker.response.collision_state);
+    } else {
+        ROS_ERROR("Failed to call service CollisionChecker");
+        return 1;
+    }
+
+    // Test Collision Predictor
+    srvPredictor.request.ref_joint_states = jointStates;
+    if (cliCollisionPredictor.call(srvPredictor)) {
+        ROS_INFO("Collision State: %d", srvChecker.response.collision_state);
+    } else {
+        ROS_ERROR("Failed to call service CollisionPredictor");
+        return 1;
+    }
+
+    return 0;
+}
---
+++ sawyer_safety_checker_srv/sawyer_safety_checker_srv.cpp
@@ -0,0 +1,92 @@
+//
+// Created by hejia on 6/28/18.
+//
+
+#include <ros/ros.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/kinematic_constraints/utils.h>
+#include <moveit/robot_state/conversions.h>
+#include <moveit/collision_detection/collision_tools.h>
+#include <moveit_msgs/PlanningScene.h>
+#include <shape_msgs/SolidPrimitive.h>
+
+#include "sawyer_safety_checker/CollisionChecker.h"
+#include "sawyer_safety_checker/CollisionPredictor.h"
+
+// Base planning scene.
+planning_scene::PlanningScenePtr pPlanningSceneParent;
+// Base planning scene + additional objects actually used to check the collision.
+planning_scene::PlanningScenePtr pPlanningScene;
+
+void onJointStatesMsg(const sensor_msgs::JointState& msg) {
+    // Update current state:
+    robot_state::RobotState& currentState = pPlanningSceneParent->getCurrentStateNonConst();
+    robot_state::jointStateToRobotState(msg, currentState);
+}
+
+bool predictCollision(sawyer_safety_checker::CollisionPredictor::Request &req,
+                      sawyer_safety_checker::CollisionPredictor::Response &res) {
+    /*
+     * sensor_msgs::JointState ref_joint_states
+     * ---
+     * std_msgs/Bool collision_state
+     */
+    robot_state::RobotState state(pPlanningScene->getCurrentState());
+    robot_state::jointStateToRobotState(req.ref_joint_states, state);
+
+    collision_detection::CollisionRequest colReq;
+    collision_detection::CollisionResult colRes;
+    pPlanningScene->checkCollision(colReq, colRes, state);
+
+    if (colRes.collision) {
+        res.collision_state.data = false;
+    }
+
+    return true;
+}
+
+bool checkCollision(sawyer_safety_checker::CollisionChecker::Request &req,
+                    sawyer_safety_checker::CollisionChecker::Response &res) {
+    /*
+     * std_msgs/Empty req
+     * ---
+     * std_msgs/Bool collision_state
+     */
+    robot_state::RobotState state(pPlanningScene->getCurrentState());
+
+    collision_detection::CollisionRequest colReq;
+    collision_detection::CollisionResult colRes;
+    pPlanningScene->checkCollision(colReq, colRes, state);
+
+    if (colRes.collision) {
+        res.collision_state.data = false;
+    }
+
+    return true;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "sawyer_collision_checker_node");
+
+    ros::NodeHandle n;
+
+    // Load robot model from ros parameter server.
+    robot_model_loader::RobotModelLoader robotModelLoader = robot_model_loader::RobotModelLoader();
+    robot_model::RobotModelPtr kinematicModel = robotModelLoader.getModel();
+    pPlanningSceneParent = planning_scene::PlanningScenePtr(new planning_scene::PlanningScene(kinematicModel));
+    pPlanningScene = pPlanningSceneParent->diff();
+
+    // Start collision predicting service.
+    ros::ServiceServer srvCollisionPredict = n.advertiseService("predict_collision", &predictCollision);
+    // Start collision checking service.
+    ros::ServiceServer srvCollisionCheck = n.advertiseService("check_collision", &checkCollision);
+
+    // Joint state updater.
+    ros::Subscriber subJointStates = n.subscribe("/robot/joint_states", 1, &onJointStatesMsg);
+
+    ros::spin();
+
+    return 0;
+}
+
